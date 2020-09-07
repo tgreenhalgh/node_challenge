a6dfff9dbf7c2b37de58ab7895773106122729be
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2020-09-07 11:57:40 -0700

    testing live API - need to add nock

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
-const express = require('express')
-const fs = require('fs')
+/** @format */
+const express = require('express');
+const bent = require('bent');
+const getJSON = bent('json');
+const semver = require('semver');
+const path = require('path');
+const fs = require('fs');
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
+const API_URL = 'https://nodejs.org/dist/index.json'
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+/**
+ * Returns an object with latest versions grouped by major version.
+ *
+ * @param {string} url The API url.
+ * @param {boolean} secure Whether we want secure versions.
+ */
+const getLatestVersion = async (url, secure) => {
+  try {
+    const resObj = {};
+    const distObj = await getJSON(url);
+    let dists = distObj;
+
+    if (secure) dists = distObj.filter((d) => d.security === true);
+
+    dists.forEach((v) => {
+      const major = `v${semver.major(v.version)}`;
+      // note that spec shows an empty files array
+      v.files = [];
+      if (resObj[major])
+        resObj[major] = semver.gt(resObj[major].version, v.version)
+          ? resObj[major]
+          : v;
+      else resObj[major] = v;
+    });
+
+    return resObj;
+  } catch (err) {
+    return err;
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('<br> Node.js Examples Initiative Challenge <br>');
+});
 
 app.get('/dependencies', (req, res) => {
   try {
-    const rawdata = fs.readFileSync('package.json', 'utf8')
-    const data = JSON.parse(rawdata)
-    res.send(data.dependencies)
+    const rawdata = fs.readFileSync('package.json', 'utf8');
+    const data = JSON.parse(rawdata);
+    const dependencies = {};
+    for (const key in data.dependencies) {
+      dependencies[key] = { name: key, version: data.dependencies[key] };
+    }
+    res.render('dependencies', { dependency: dependencies });
   } catch (err) {
-    console.error(`Error reading package.json: ${err}`)
+    console.error(`Error reading package.json: ${err}`);
+    res.send(err);
   }
-})
+});
 
+app.get('/minimum-secure', async (req, res) => {
+  res.json(
+    await getLatestVersion(API_URL, true),
+  );
+});
+
+app.get('/latest-releases', async (req, res) => {
+  res.json(
+    await getLatestVersion(API_URL, false),
+  );
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Examples Initiative challenge app listening at http://localhost:${PORT}`)
+// })
 app.listen(PORT)
 
 // necessary for tape testing
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+/** @format */
 const tape = require('tape')
 const bent = require('bent')
 const getPort = require('get-port')
@@ -12,22 +13,36 @@ const getBuffer = bent('buffer')
 const context = {}
 
 tape('setup', async function (t) {
-	const port = await getPort()
-	context.server = server.listen(port)
-	context.origin = `http://localhost:${port}`
+  const port = await getPort()
+  context.server = server.listen(port)
+  context.origin = `http://localhost:${port}`
 
-	t.end()
+  t.end()
 })
 
 tape('should get dependencies', async function (t) {
-	const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
-
-	// assertions etc
+  const html = (await getBuffer(`${context.origin}/dependencies`)).toString()
+  t.true(html.includes('bent'), 'should contain bent')
+  t.true(html.includes('express'), 'should contain express')
+  t.true(html.includes('hbs'), 'should contain hbs')
+  t.end()
 })
 
-// more tests
+tape('should get minimum secure versions', async function (t) {
+  const json = await getJSON(`${context.origin}/minimum-secure`)
+  t.true(json.v0.version === 'v0.12.17', 'v0 version shold match')
+  t.true(json.v4.version === 'v4.9.0', 'v4 version shold match')
+  t.end()
+})
+
+tape('should get latest-releases', async function (t) {
+  const json = await getJSON(`${context.origin}/latest-releases`)
+  t.true(json.v14.version === 'v14.9.0', 'v14 version shold match')
+  t.true(json.v13.version === 'v13.14.0', 'v13 version shold match')
+  t.end()
+})
 
 tape('teardown', function (t) {
-	context.server.close()
-	t.end()
+  context.server.close()
+  t.end()
 })
